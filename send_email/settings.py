967fcb28fe3a63db3e7886467ffbe6cbb11c20b2
User <natali29915021@mail.ru>
2020-09-30 13:00:52 +0300

    add

--- send_email/settings.py
+++ send_email/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import django_heroku
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -132,7 +130,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
-#django_heroku.settings(locals())
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
